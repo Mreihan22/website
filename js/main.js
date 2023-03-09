4a00805eb29b832674dbeb4b9d0178c5ce8eee67
Muhammad Reihan <125675696+Mreihan22@users.noreply.github.com>
2023-03-10 00:00:57 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const masuk = confirm("ANDA YAKIN MAU MASUK?");
+
+if(masuk){
+    const name = prompt("SIAPA NAMA ANDA?");
+    alert(`Hello ${name} welcome to my website`);
+}else{
+    alert("YAHHHH, KO GA MSUK SI");
+}
